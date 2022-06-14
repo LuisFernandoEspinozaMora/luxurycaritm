acf3d6d8b028e1da8f7d0e75c9110ec08b1a696a
Cristobal Pérez García <cristobal.pg16@gmail.com>
2022-06-14 14:09:50 -0600

    Cambios de contacto

--- page-contacto.php
+++ page-contacto.php
@@ -1,36 +1,58 @@
 <?php get_header(); ?>
 
 
-<main class="contenedor">
-    <form class="formulario">
+<main>
 
+    <div class="hero formulario text-center">
         <h1>contactenos</h1>
+    </div>
+
+
+    <div class="container information">
+        <div class="row text-center">
+            <div class="col">
+                <h3 class="fas fa-map-marker"></h3>
+                <p><span>Dirección:</span></p>
+                <p> 700 metros sur de Multiplaza Escazú</p>
+            </div>
+            <div class="col">
+                <h3 class="fas fa-phone-alt"></h3>
+                <p><span>Showroom de ventas Tel:</p>
+                <p></span> 2547 5145</p>
+            </div>
+            <div class="col">
+                <h3 class="fab fa-whatsapp"></h3>
+                <p><span>WhatsApp taller Tel:</span></p>
+                <p> +506 8529 3842</p>
+            </div>
+        </div>
+    </div>
+
+    <form class="contenedor formulario">
+
+        <h3 class="text-center">llena esto si tienes alguna duda</h3>
+
         <p>(<span>*</span>) campos obligatorios</p>
 
-        <div class="contact-area">
+        <div class="">
 
-            <div class="fieldset-area">
+            <div>
 
-                <fieldset>
-
-
+                <fieldset class="fieldset-area">
                     <div class="form-group">
-                        <label for="asunto">Asunto</label>
-                        <input type="text" class="form-control" id="asunto">
+                        <!--<label for="asunto">Asunto</label>-->
+                        <input type="text" class="form-control" id="asunto" placeholder="Asunto">
                     </div>
 
                     <div class="form-group">
-                        <label for="nombre">Nombre <span>*</span></label>
-                        <input type="text" class="form-control" id="nombre" required>
+                        <input type="text" class="form-control" id="nombre" required placeholder="Nombre *">
                     </div>
 
                     <div class="form-group">
-                        <label for="telefono">Teléfono <span>*</span></label>
-                        <input type="tel" class="form-control" id="telefono" required>
+                        <input type="tel" class="form-control" id="telefono" required placeholder="Teléfono *">
                     </div>
 
                     <div class="form-group">
-                        <label for="exampleFormControlSelect1">Sucursal</label>
                         <select class="form-control" id="exampleFormControlSelect1" required>
                             <option value="SJ" disabled selected>Seleccione una opción</option>
                             <option value="SJ">San Jose</option>
@@ -43,34 +65,24 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="exampleFormControlInput1">E-mail <span>*</span></label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" required>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" required placeholder="E-mail *">
                     </div>
 
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Mensaje</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" cols="20" rows="3"></textarea>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" cols="20" rows="3" placeholder="Mensaje"></textarea>
                     </div>
 
                 </fieldset>
                 <div class="btn_centrado"><button type="submit" class="btn btn-luxury">Enviar</button></div>
             </div>
 
-
-            <div class="information">
-                <p><span>Dirección:</span> 700 metros sur de Multiplaza Escazú</p>
-
-                <p><span>Showroom de ventas Tel:</span> 2547-5145</p>
-
-                <p><span>Servicio de taller Tel:</span> 2547-5145</p>
-
-                <p><span>WhatsApp taller Tel:</span> +506 8529 3842</p>
-            </div>
-
         </div>
 
-       
+
     </form>
+
+
+
 </main>
 
 
